19c37f85798634042670f37c2a8960cebb8b8481
Darya Litvinova <lit.darya876@gmail.com>
2024-07-08 14:16:51 +0200

    add files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, Hexlet!");
+import _ from 'lodash';
+
+const object = { a: [{ b: { c: 3 } }] };
+
+let obj;
+beforeEach(() => {
+  obj = _.cloneDeep(object);
+});
+
+test("first flow", () => {
+  _.set(obj, 'a[0].b.c', 6);
+  expect(obj.a[0].b.c).toEqual(6);
+});
+
+test("second flow", () => {
+  _.set(obj, ['x', '0', 'y', 'z'], 8);
+  expect(obj.x[0].y.z).toEqual(8);
+});
